304e93ff27a2535419d0ec3463cc6c9734c94131
Hilmy Tsany <82049652+hyperk1ll@users.noreply.github.com>
2022-06-05 09:19:03 +0700

    Calculate BMI

---
+++ 8 kyu/CalculateBMI.py
@@ -0,0 +1,13 @@
+# Calculate BMI
+
+def bmi(weight, height):
+    if (weight / height**2 <= 18.5):
+        return "Underweight"
+    elif (weight / height**2 <= 25):
+        return "Normal"
+    elif (weight / height**2 <= 30):
+        return "Overweight"
+    elif (weight / height**2 > 30):
+        return "Obese"
+    else:
+        return "You are not human"
