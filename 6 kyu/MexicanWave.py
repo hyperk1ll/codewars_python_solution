5cd0f0c202b3682b1c7e24bf5dee0268166621c1
Hilmy Tsany <82049652+hyperk1ll@users.noreply.github.com>
2022-06-05 08:50:21 +0700

    Mexican Wave

--- 6 kyu/MexicanWave.py
+++ 6 kyu/MexicanWave.py
@@ -5,7 +5,7 @@
 The wave (known as the Mexican wave in the English-speaking world outside North America) is 
 an example of metachronal rhythm achieved in a packed stadium when successive groups of spectators 
 briefly stand, yell, and raise their arms. Immediately upon stretching to full height, the spectator 
-returns to the usual seated position.
+returns to the usual seated position. 
 
 The result is a wave of standing spectators that travels through the crowd, even though individual 
 spectators never move away from their seats. In many large arenas the crowd is seated in a contiguous 
