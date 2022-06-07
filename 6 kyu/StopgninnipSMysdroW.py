756ce4bcb0a6cbc4191d22082172353008967b9c
Hilmy Tsany <82049652+hyperk1ll@users.noreply.github.com>
2022-06-07 22:10:26 +0700

    Stop gninnipS My sdroW!

--- 6 kyu/StopgninnipSMysdroW.py
+++ 6 kyu/StopgninnipSMysdroW.py
@@ -1,5 +1,6 @@
+# Stop gninnipS My sdroW!
 
-
+# Answer 1
 def spin_words(sentence):
     words = sentence.split()
     
@@ -8,3 +9,7 @@ def spin_words(sentence):
             words[word] = i[::-1]
         
     return ' '.join(words)
+
+# Answer 2
+def spin_words(sentence):
+    return ' '.join(x[::-1] if len(x) > 4 else x for x in sentence.split())
