1dc3ff66e9761c3788d9bc867a5fe30055b4726e
Hilmy Tsany <82049652+hyperk1ll@users.noreply.github.com>
2022-06-13 21:32:07 +0700

    Make a function that does arithmetic!

---
+++ 7 kyu/MakeFunctionThatDoesArithmetic.py
@@ -0,0 +1,11 @@
+# Make a function that does arithmetic!
+
+def arithmetic(a, b, operator):
+    if operator == "add":
+        return a + b
+    elif operator == "subtract":
+        return a - b
+    elif operator == "multiply":
+        return a * b
+    elif operator == "divide":
+        return a / b
