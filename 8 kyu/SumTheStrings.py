87767b81b0691c97f16cdacc3ce55ae310de9f2a
Hilmy Tsany <82049652+hyperk1ll@users.noreply.github.com>
2022-06-13 22:45:24 +0700

    Sum The Strings

---
+++ 8 kyu/SumTheStrings.py
@@ -0,0 +1,11 @@
+# Sum The Strings
+
+def sum_str(a, b):
+    if a == "" and b == "":
+        return "0"
+    elif a == "":
+        return b
+    elif b == "":
+        return a
+    else:
+        return str(int(a) + int(b))
